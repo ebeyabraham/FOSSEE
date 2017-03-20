086e55dc9a3eaecb6942e3999588c5a67f1612c3
Ebey Abraham <ebey97@gmail.com>
2017-03-20 18:03:12 +0000

    face.cpp edited online with Bitbucket

--- SectionI/Task1/face.cpp
+++ SectionI/Task1/face.cpp
@@ -1,3 +1,16 @@
+/*
+Name: Ebey Abraham
+The following is my solution to the first task of the IP/CV internship evaluation
+The code is genuine and has been written by me. 
+I have explained my algorithm throughout the code through comments
+
+The code was tested using OpenCV 3.2.0
+
+Sources used for reference:
+1. http://docs.opencv.org/2.4/doc/tutorials/objdetect/cascade_classifier/cascade_classifier.html
+2. http://docs.opencv.org/2.4/modules/objdetect/doc/cascade_classification.html
+*/
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
@@ -99,6 +112,7 @@ int main( int argc, const char** argv )
 		
 		vector<Rect> cat;
 		cvtColor(img,gray,CV_BGR2GRAY);
+        
         /*
         To detect Ellen DeGeneres from the image I have used the frontalcatface cascade classifier.
         frontalcatface sometimes mistakes human faces as cats, this is mentioned in the comment section of the repository in Github
@@ -115,18 +129,19 @@ int main( int argc, const char** argv )
      		int Y=cat[i].y;
      		int W=cat[i].width;
      		int H=cat[i].height;
-     		
+     		// Draw a blue rectangle around Ellen's face
     		rectangle(image,Point(X, Y),Point(X+W, Y+H),Scalar(255, 0, 0),3);
     		
     		Mat ROI=gray(cat[i]);
     		vector<Rect> eyes;
     		eye_cascade.detectMultiScale(ROI,eyes,1.1,2,0|CV_HAAR_SCALE_IMAGE,Size(30,30));
-    		cout<<"BGR values of Ellen's eyes:\n";
+    		cout<<"BGR values of centroid of Ellen's eyes:\n";
     		for( size_t j = 0; j < eyes.size(); j++ )
      		{
        		int X=cat[i].x + eyes[j].x + eyes[j].width*0.5;
        		int Y=cat[i].y + eyes[j].y + eyes[j].height*0.5;
        		Point center(X,Y);
+            // Print BGR values of the centroid of Ellen's eyes
        		cout<<image.at<Vec3b>(Y,X)<<endl;
        		int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
        		circle( image, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
